c95c6632c2e1ba116b98fe499a59146a44d12e8a
Adam Ceylan <eadamceylan@gmail.com>
2022-05-24 23:45:01 -0400

    selenium2022 12

--- src/test/java/com/myfirstproject/Day10_ScreenshotOfSpecificElements.java
+++ src/test/java/com/myfirstproject/Day10_ScreenshotOfSpecificElements.java
@@ -1,5 +1,4 @@
 package com.myfirstproject;
-
 import com.myfirstproject.utilities.TestBase;
 import org.apache.commons.io.FileUtils;
 import org.junit.Test;
@@ -8,15 +7,22 @@ import org.openqa.selenium.OutputType;
 import org.openqa.selenium.WebElement;
 import java.io.File;
 import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 public class Day10_ScreenshotOfSpecificElements extends TestBase {
     @Test
     public void screenshotTest() throws IOException {
         driver.get("https://www.google.com");
         WebElement googleLogo = driver.findElement(By.xpath("//img[@class='lnXdpd']"));
         File logoImage = googleLogo.getScreenshotAs(OutputType.FILE);
-
 //        Saving the image to  the path
-        File finalPath = new File("googleLogo.png");
+//        String currentDate = new SimpleDateFormat("yyyyMMddhhmmss").format(new Date());
+        File finalPath = new File("./test-output/elements/googleLogo.png");
         FileUtils.copyFile(logoImage,finalPath);
+//        getting screenshot of sign in element
+        WebElement signInButton = driver.findElement(By.xpath("//*[.='Sign in']"));
+        File signInImage = signInButton.getScreenshotAs(OutputType.FILE);
+        File signInPath = new File("./test-output/elements/signIn.png");
+        FileUtils.copyFile(signInImage,signInPath);
     }
 }

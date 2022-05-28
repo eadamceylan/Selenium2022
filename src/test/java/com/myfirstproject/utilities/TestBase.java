fe073830cfa3ee8f646bb5c3560624813ab957a1
Adam Ceylan <eadamceylan@gmail.com>
2022-05-27 22:30:33 -0400

    selenium2022 13

--- src/test/java/com/myfirstproject/utilities/TestBase.java
+++ src/test/java/com/myfirstproject/utilities/TestBase.java
@@ -1,4 +1,7 @@
 package com.myfirstproject.utilities;
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.ExtentTest;
+import com.aventstack.extentreports.reporter.ExtentHtmlReporter;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.apache.commons.io.FileUtils;
 import org.junit.After;
@@ -25,12 +28,41 @@ public abstract class TestBase {
      *
      * */
     protected WebDriver driver;
+    protected ExtentReports extentReports;
+    protected ExtentHtmlReporter extentHtmlReporter;
+    protected ExtentTest extentTest;
     @Before
     public void setUp(){
         WebDriverManager.chromedriver().setup();
         driver = new ChromeDriver();
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
         driver.manage().window().maximize();
+
+        //Set the path => creates a reports folder under the test-output folder
+        String currentDate = new SimpleDateFormat("yyyyMMddhhmmss").format(new Date());
+        String path = System.getProperty("user.dir")+"/test-output/report/"+currentDate+"test_report.html";
+
+//        Create extentHtmlReporter and set the path inside it
+        extentHtmlReporter = new ExtentHtmlReporter(path);
+
+//        Create extent report
+        extentReports = new ExtentReports();
+
+//        Add custom information
+        extentReports.setSystemInfo("Environment","Test Environment");
+        extentReports.setSystemInfo("Browser","Chrome");
+        extentReports.setSystemInfo("Application","Techproed");
+        extentReports.setSystemInfo("SQA","John");
+
+        extentHtmlReporter.config().setDocumentTitle("Techproed QA's");
+        extentHtmlReporter.config().setReportName("Techproed Extent Report");
+
+//        attach html and extent reports
+        extentReports.attachReporter(extentHtmlReporter);
+
+//        Report is completed. We need to create test using extentTest object
+        extentTest = extentReports.createTest("My Project Extent Reports","This is a report for smoke test");
+
     }
     @After
     public void tearDown(){
